7dba4ab1c5330458748368b09040507b8529de13
teaho2015 <teaho2015@gmail.com>
2017-09-21 05:12:03 +0800

    Add OutOfMemoryError module.

---
+++ javacode/src/main/java/com/tea/outofmemory/MetaspaceMain2.java
@@ -0,0 +1,32 @@
+/**
+ *
+ */
+package com.tea.outofmemory;
+
+/**
+ * copy from https://plumbr.eu/outofmemoryerror
+ *
+ * test java.lang.OutOfMemoryError: Metaspace
+ *
+ */
+public class MetaspaceMain2 {
+    static javassist.ClassPool cp = javassist.ClassPool.getDefault();
+
+
+    public static void main(String[] args) throws Exception{
+        for (int i = 0; ; i++) {
+            Class c = cp.makeClass("com.tea.outofmemory.metaspace" + i).toClass();
+        }
+    }
+
+}
+
+
+/*//java8 output
+Exception in thread "main" java.lang.OutOfMemoryError: Metaspace
+	at javassist.ClassPool.toClass(ClassPool.java:1085)
+	at javassist.ClassPool.toClass(ClassPool.java:1028)
+	at javassist.ClassPool.toClass(ClassPool.java:986)
+	at javassist.CtClass.toClass(CtClass.java:1079)
+	at com.tea.outofmemory.MetaSpaceMain2.main(MetaSpaceMain2.java:16)
+ */
